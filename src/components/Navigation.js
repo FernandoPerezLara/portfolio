811bc309db7447ee9b5b6cdeabdaee6419748241
Fernando Perez Lara <fernando.perez.lara@outlook.com>
2020-12-27 16:18:02 +0100

    Navigation menu deleted

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
 import "../styles/navigation.scss";
 
-import Home from "../pages/Home";
+import Home from "../pages/Home/Home";
 import About from "../pages/About";
 import Skills from "../pages/Skills";
 import Contact from "../pages/Contact";
@@ -12,15 +12,6 @@ import NotFound from "../pages/NotFound";
 export default function Navigation() {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/skills">Skills</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-      </nav>
-
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
