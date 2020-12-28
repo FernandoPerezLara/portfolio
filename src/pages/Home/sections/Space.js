cdff94821628c59deba5be588c921355d6d13449
Fernando Perez Lara <fernando.perez.lara@outlook.com>
2020-12-28 21:59:58 +0100

    Slot effect implemented

--- src/pages/Home/sections/Space.js
+++ src/pages/Home/sections/Space.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Particles from "react-particles-js";
+import $ from "jquery";
 
 export default function Space() {
   const [touchable, setTouchable] = useState(true);
@@ -65,8 +66,69 @@ export default function Space() {
       }} />
       <div className="presentation">
         <div>Hello, I'm <span>Fernando Perez</span>.</div>
-        <div>I'm a simple developer.</div>
+        <div>I'm a <SlotText /> developer.</div>
       </div>
+    </div>
+  );
+}
+
+function SlotText() {
+  const wordList = ["React", "Python", "C#", "NodeJS", "JavaScript", "React Native", "C", "Visual Basic", "Arduino", "MATLAB", "SQL", "Angular", "R", "Verilog", "Go", "TypeScript", "Tensorflow"];
+  const container = $("#slotText .slotContainer");
+  const [height, setHeight] = useState(48);
+
+  useEffect(() => {
+    buildSlot();
+    slotHeight();
+
+    function slotHeight() {
+      setHeight($(".slotItem").height());
+    }
+
+    window.addEventListener("resize", slotHeight);
+
+    const interval = setInterval(setAnimation, 2000);
+    
+    return () => clearInterval(interval);
+  });
+
+  const buildSlot = () => {
+    const items = wordList.map(buildItem);
+
+    container.append(items);
+  };
+
+  const buildItem = (text) => {
+    return $("<div>").addClass("slotItem").text(text);
+  };
+
+  const setAnimation = () => {
+    var index = Math.floor(Math.random() * (wordList.length - (~~(wordList.length / 3)))) + (~~(wordList.length / 3));
+
+    $("#slotText").height(height);
+
+    container.animate({top: -index*height}, 500, "swing", function() {
+      setTimeout(function () {
+        pushItems(container, index);
+        
+        container.css({top: 0});
+      }, 300);
+    });
+  };
+
+  const pushItems = (container, index) => {
+    var children = container.find(".slotItem");
+
+    children.slice(0, index).insertAfter(children.last());
+
+    if (index === children.length) {
+      pushItems(container, 1);
+    }
+  }
+
+  return (
+    <div className="slotMask" id="slotText">
+      <div className="slotContainer"></div>
     </div>
   );
 }
