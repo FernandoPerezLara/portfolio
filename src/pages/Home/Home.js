5ca3ef52264662411c7cd5cca9f46878e4d7bafa
Fernando Perez Lara <fernando.perez.lara@outlook.com>
2021-01-12 15:31:36 +0100

    Autoscroll removed

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -15,12 +15,32 @@ export default function Home(props) {
   return (
     <ReactFullpage
       scrollingSpeed={1000}
+
       render={({ state, fullpageApi }) => {
         return (
           <ReactFullpage.Wrapper>
             <Space fullpageApi={fullpageApi} />
             <div className="section">
-              <p>Section 2</p>
+              <div style={{height: "100px"}}>Some text 1</div>
+              <div style={{height: "100px"}}>Some text 2</div>
+              <div style={{height: "100px"}}>Some text 3</div>
+              <div style={{height: "100px"}}>Some text 4</div>
+              <div style={{height: "100px"}}>Some text 5</div>
+              <div style={{height: "100px"}}>Some text 6</div>
+              <div style={{height: "100px"}}>Some text 7</div>
+              <div style={{height: "100px"}}>Some text 8</div>
+              <div style={{height: "100px"}}>Some text 9</div>
+              <div style={{height: "100px"}}>Some text 10</div>
+              <div style={{height: "100px"}}>Some text 11</div>
+              <div style={{height: "100px"}}>Some text 12</div>
+              <div style={{height: "100px"}}>Some text 13</div>
+              <div style={{height: "100px"}}>Some text 14</div>
+              <div style={{height: "100px"}}>Some text 15</div>
+              <div style={{height: "100px"}}>Some text 16</div>
+              <div style={{height: "100px"}}>Some text 17</div>
+              <div style={{height: "100px"}}>Some text 18</div>
+              <div style={{height: "100px"}}>Some text 19</div>
+              <div style={{height: "100px"}}>Some text 20</div>
             </div>
             <div className="section">
               <p>Section 3</p>
@@ -31,6 +51,8 @@ export default function Home(props) {
           </ReactFullpage.Wrapper>
         );
       }}
+      scrollBar={true}
+      autoScrolling={false}
       onLeave={() => setIsOpen(false)}
     />
   );
