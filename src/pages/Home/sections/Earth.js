35ce862a5a6bff77d98a8bfa85e95c04729372bf
Fernando Perez Lara <fernando.perez.lara@outlook.com>
2021-01-26 20:44:34 +0100

    Earth created but unfinished

--- src/pages/Home/sections/Earth.js
+++ src/pages/Home/sections/Earth.js
@@ -3,7 +3,10 @@ import React from "react";
 export default function Earth() {
   return (
     <div className="section" id="earth">
-      
+      <div className="horizon">
+        <div className="glow"></div>
+      </div>
+      <div className="earth"></div>
     </div>
   );
 }
